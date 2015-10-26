7fd4f7123c0f57d7e7cc1fbd7601828f956f4b06
Artem Poletsky <artempoletsky@gmail.com>
2015-10-26 20:02:07 +0500

    api refactor

--- src/model.js
+++ src/model.js
@@ -1,3 +1,57 @@
-var API = Class.create({
+var FD = require('form-data');
+var fs = require('fs');
 
+
+var API = Class.create({
+    domain: 'https://2ch.hk/',
+    getThreads: function (board, callback) {
+        $.get(this.domain + '/' + board + '/catalog.json', callback);
+    },
+    chooseFiles: function (num, subfolder) {
+        var files = fs.readdirSync('./files/' + subfolder);
+        var names = [];
+        var dir = process.cwd() + '/files/' + subfolder + '/';
+        var filesTotal = files.length;
+        for (var i = 0; i < num; i++) {
+            names.push(dir + files[Math.floor(filesTotal * Math.random())]);
+        }
+        return names;
+
+    },
+    postInThread: function (data, files, callback) {
+        var self = this;
+        var fd = new FD();
+
+        _.each(data, function (value, key) {
+            fd.append(key, value);
+        });
+
+        _.each(files, function (name, key) {
+            fd.append('image' + key, fs.createReadStream(name))
+        });
+
+
+        var url = self.domain + 'makaba/posting.fcgi?json=1'
+
+        fd.submit({
+            host: '2ch.hk',
+            path: '/makaba/posting.fcgi?json=1',
+            headers: {
+                'Accept': '*/*',
+                //'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryaHm5pJod8blkud3H',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'
+            }
+        }, function (err, res) {
+            var body = '';
+            res.on('data', function (chunk) {
+                body += chunk;
+            });
+            res.on('end', function () {
+
+                callback(body);
+
+            });
+        });
+
+    },
 });
--- src/view.js
+++ src/view.js
@@ -1,7 +1,5 @@
 ﻿var gui = require('nw.gui');
-var fs = require('fs');
 
-var FD = require('form-data');
 
 $.fn.serialize = function () {
     return _.foldl(this.serializeArray(), function (result, object) {
@@ -33,8 +31,6 @@ ViewModel.create({
         }
 
 
-        //this.postInThread('kpop', '177');
-
     },
     posted: [],
     watchBoard: function () {
@@ -42,7 +38,10 @@ ViewModel.create({
         var fn = function () {
             self.findWebmThread(function (op, subfolder) {
                 var thread = op.num;
-                self.postInThread('b', thread, subfolder);
+                var data = self.$form.serialize();
+                data.board = 'b';
+                data.thread = thread;
+                API.postInThread(data, API.chooseFiles(1, subfolder));
             });
         };
 
@@ -51,7 +50,7 @@ ViewModel.create({
     },
     findWebmThread: function (callback) {
         var self = this;
-        this.getThreads('b', function (data) {
+        API.getThreads('b', function (data) {
             /*
              var ops = _.map(data.threads, function (thread) {
              return thread.posts[0];
@@ -124,31 +123,18 @@ ViewModel.create({
             //  console.log(subj);
         }
     },
-    chooseFile: function (subfolder, callback) {
 
-        var self = this;
-        //console.log();
-        fs.readdir('./files/' + subfolder, function (err, files) {
-            var filename = files[Math.floor(files.length * Math.random())];
-            var fullFileName = process.cwd() + '/files/' + subfolder + '/' + filename;
-            self.log('File chosen: ' + fullFileName);
-            callback(fullFileName);
-            //console.log(fullFileName);
-            //self.$file[0].value =fullFileName;
-            //console.log(filename);
-        });
-    },
     openLink: function (e) {
         e.preventDefault();
         gui.Shell.openExternal(this.link);
     },
-    domain: 'https://2ch.hk/',
+
     getCaptcha: function (callback) {
         var self = this;
-        $.get(self.domain + 'makaba/captcha.fcgi?type=2chaptcha&action=thread', function (data) {
+        $.get(API.domain + 'makaba/captcha.fcgi?type=2chaptcha&action=thread', function (data) {
             var id = data.split(/\n/)[1];
             if (id) {
-                self.captcha = self.domain + 'makaba/captcha.fcgi?type=2chaptcha&action=image&id=' + id;
+                self.captcha = API.domain + 'makaba/captcha.fcgi?type=2chaptcha&action=image&id=' + id;
             } else {
                 self.captcha = undefined;
                 callback();
@@ -158,55 +144,11 @@ ViewModel.create({
     log: function (text) {
         this.$log.val(this.$log.val() + text + '\n');
     },
-    getThreads: function (board, callback) {
-        $.get(this.domain + '/' + board + '/catalog.json', callback);
-    },
-    postInThread: function (board, thread, subfolder) {
-        var self = this;
-        var fd = new FD();
-        fd.append('board', board);
-        fd.append('thread', thread);
 
-        _.each(self.$form.serialize(), function (value, key) {
-            fd.append(key, value);
-        });
-
-        //console.log(fd.get('image1'));
-        self.chooseFile(subfolder, function (name) {
-            fd.append('image1', fs.createReadStream(name));
-
-            //console.log(fd);
-            //return;
-            var url = self.domain + 'makaba/posting.fcgi?json=1'
-
-            self.log('Post start: board = ' + board + 'thread = ' + thread);
-            fd.submit({
-                host: '2ch.hk',
-                path: '/makaba/posting.fcgi?json=1',
-                headers: {
-                    'Accept': '*/*',
-                    //'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryaHm5pJod8blkud3H',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36'
-                }
-            }, function (err, res) {
-                var body = '';
-                res.on('data', function (chunk) {
-                    body += chunk;
-                });
-                res.on('end', function () {
-                    self.posted.push(thread);
-                    fs.writeFileSync('./posted.json', JSON.stringify(self.posted));
-                    self.log('Post end: ' + body);
-                });
-            });
-        });
-        //return;
-
-    },
     initialize: function () {
         var self = this;
         self.getCaptcha(function () {
-            self.watchBoard();
+            //self.watchBoard();
         });
 
         this.posted = JSON.parse(fs.readFileSync('./posted.json', 'utf-8'));
@@ -215,13 +157,23 @@ ViewModel.create({
 
         var win = this.win = gui.Window.get();
 
-        var menu = new gui.Menu();
-        menu.append(new gui.MenuItem({type: 'checkbox', label: 'box1'}));
-        tray.menu = menu;
+        /*var menu = new gui.Menu();
+         menu.append(new gui.MenuItem({type: 'checkbox', label: 'box1'}));
+         tray.menu = menu;*/
 
 
         tray.on('click', function () {
             win.show();
+        });
+
+        var data = self.$form.serialize();
+        data.board = 'kpop';
+        data.thread = '14395';
+        data.comment = 'test';
+        API.postInThread(data, API.chooseFiles(2, 'default'), function (data) {
+            self.log('Post end: ' + data);
+            //self.posted.push(thread);
+            //fs.writeFileSync('./posted.json', JSON.stringify(self.posted));
         });
     }
 });
